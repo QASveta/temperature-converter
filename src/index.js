7dec1ba25d1179ef7c89d9b3ec2af4015c235d0b
svetlana pazyko <svetlanapazyko@MacBook-Pro-svetlana.local>
2020-01-20 15:41:38 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 
 
 exports.cToF = function (c) {
-
+    let cTemp = c;
+    let cToFahr = cTemp * 9 / 5 + 32;
+    return cToFahr.toFixed(2)
 }
 
 exports.fToC = function (f) {
-
+    let fTemp = f;
+    let fToCel = (fTemp - 32) * 5 / 9;
+    return fToCel.toFixed(2)
 }
